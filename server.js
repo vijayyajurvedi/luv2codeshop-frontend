a0f8c1c06b460da2cab970e441cf30df021f36fc
Vijay Yajurvedi <vijay.yajurvedi@gmail.com>
2021-03-22 13:04:14 +0530

    first deploy

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// server.js
 const express = require('express');
 const app = express();
-const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static('./dist/luv2codeshop-frontend'));
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  // Instruct the app
+  // to use the forceSSL
+  // middleware
+  app.use(forceSSL());
 
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/luv2codeshop-frontend/'}
-  );
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
+  
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
